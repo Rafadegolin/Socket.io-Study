e71460745f2c35c45fd820194b185cea699178bf
Rafadegolin <rafaeldegolin26@hotmail.com>
2024-10-28 17:51:38 -0300

    atualizações

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// Chat
+// io.on("connection", (socket) => {
+//   console.log("usuário conectado");
+//   socket.on("chat message", (msg) => {
+//     io.emit("chat message", msg);
+//   });
+// });
+
 io.on("connection", (socket) => {
-  console.log("usuário conectado");
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  console.log("Usuário conectado");
+
+  // Escuta o evento 'barcode'
+  socket.on("barcode", (code) => {
+    console.log("Código de barras recebido: ", code);
+
+    // Emite agora o 'barcode' para as conexões
+    io.emit("barcode-received", code);
   });
 });
 
